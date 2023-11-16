8ded7f6210784a85455b0e3f7d3d7478bcb6d86f
Yves-Ishimwe <114264084+Yves-Ishimwe@users.noreply.github.com>
2023-11-16 11:12:12 +0200

    Create mcq.py

---
+++ HomeWork 3/MCQ/mcq.py
@@ -0,0 +1,25 @@
+def question_1():
+    # return ""
+    return "b"#NotImplementedError
+
+
+def question_2():
+    # return ""
+    return "c"#NotImplementedError
+
+
+def question_3():
+    # return ""
+    return "b"#NotImplementedError
+
+
+
+def question_4():
+    # return ""
+    return "b"#NotImplementedError
+
+
+def question_5():
+    # return ""
+    return "a"#NotImplementedError
+
